5c3fe3a808a1e7244054200d68e36d4b67da7c1e
Whispe <tyung1@jhu.edu>
2013-09-21 14:30:29 -0400

    Update MyBrickusFrame.java
    Working on piece tray. Sized relative horizontal widths of piece tray and button.

--- MyBrickusFrame.java
+++ MyBrickusFrame.java
@@ -11,13 +11,14 @@ public class MyBrickusFrame extends JFrame {
 	
 	public static void main(String[] args) {
 
-		BrickusModel model = new edu.jhu.cs.oose.fall2013.brickus.model.StandardBrickusModel();
+		BrickusModel model = new edu.jhu.cs.oose.fall2013.brickus.model.StandardBrickusModel(); //REVIVE: use own model class.
 		MyBrickusFrame gui = new MyBrickusFrame(model);
 		gui.go();
-		//gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		//gui.setSize(670,710);
-        //gui.setLocationRelativeTo(null);
-        //gui.setVisible(true);
+	}
+	
+	public MyBrickusFrame(BrickusModel model) {
+		
+		
 	}
 	
 	public void go() {
@@ -28,28 +29,44 @@ public class MyBrickusFrame extends JFrame {
 		// MyBrickusBoard board
 		// frame.getContentPane().add(BorderLayout.CENTER,board);
 		
-		// MyBrickusTray tray // lower panel of pieces and pass button
-		// frame.getContentPane().add(BorderLayout.CENTER, tray);
+		MyBrickusTray tray = new MyBrickusTray(); // lower panel of pieces and pass button
+		frame.getContentPane().add(BorderLayout.CENTER, tray.panel);
 		
 		MyBrickusTracker tracker = new MyBrickusTracker(); // error board, score boards
 		frame.getContentPane().add(BorderLayout.PAGE_END, tracker.panel);
 		
 		frame.setSize(670,710);
-		//frame.pack();
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
-	}
-	
-	public MyBrickusFrame(BrickusModel model) {
-		
-		
 	}
 }
 
 @SuppressWarnings("serial")
 class MyBrickusTray extends JPanel {
 	
+	JPanel panel = new JPanel(new GridBagLayout());
 	
+	public MyBrickusTray() {
+		
+		panel.setBorder(BorderFactory.createLineBorder(Color.black));		
+		GridBagConstraints constraints = new GridBagConstraints();
+		constraints.fill = GridBagConstraints.BOTH;
+		
+		JPanel holdPieceTray = new JPanel();
+		holdPieceTray.setBorder(BorderFactory.createLineBorder(Color.black));
+		constraints.gridx = 0;
+		constraints.gridy = 0;
+		constraints.gridwidth = 7;
+		constraints.weightx = 1;
+		panel.add(holdPieceTray, constraints);
+		
+		JButton passButton = new JButton("Pass");
+		constraints.gridx = 7;
+		constraints.gridy = 0;
+		constraints.gridwidth = 1;
+		constraints.weightx = 0;
+		panel.add(passButton, constraints);
+	}
 }
 
 @SuppressWarnings("serial")
@@ -72,21 +89,14 @@ class MyBrickusTracker extends JPanel {
 		errorText.setText(errorText.getText() + "<font color=006600>u</font>");
 		errorText.setText(errorText.getText() + "<font color=FF0066>r</font");
 		errorText.setText(errorText.getText() + "<font color=000000>Cats! =^.^=");
-		//errorText.setBorder(BorderFactory.createLineBorder(Color.black));
-		//JLabel errorText = new JLabel("Welcome to FourCats! =^.^=");
 		
 		JLabel player1Score = new JLabel("<html><font color=0000CC>Score: 0 </font>");
-		//player1Score.setBorder(BorderFactory.createLineBorder(Color.blue));
-		//player1Score.setForeground(Color.blue);
 		
 		JLabel player2Score = new JLabel("<html><font color = CC0000>Score: 0 </font>");
-		//player2Score.setBorder(BorderFactory.createLineBorder(Color.red));
 		
 		JLabel player3Score = new JLabel("<html><font color = 006600>Score: 0 </font>");
-		//player3Score.setBorder(BorderFactory.createLineBorder(Color.green));
 		
 		JLabel player4Score = new JLabel("<html><font color = FF0066>Score: 0 </font>");
-		//player4Score.setBorder(BorderFactory.createLineBorder(Color.magenta));
 		
 		subpanelLeft.add(errorText);
 		subpanelRight.add(player1Score);

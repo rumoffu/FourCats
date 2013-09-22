7b050fd94c67bc1352f85f94addd518a1837505d
Kyle <wong.kyle5@gmail.com>
2013-09-21 22:04:08 -0400

    Update MyBrickusFrame.java

--- MyBrickusFrame.java
+++ MyBrickusFrame.java
@@ -3,9 +3,12 @@ package edu.jhu.cs.tyung1.oose;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
 
 import javax.swing.*;
+
 
 import edu.jhu.cs.oose.fall2013.brickus.iface.BrickusEvent;
 import edu.jhu.cs.oose.fall2013.brickus.iface.BrickusIllegalMoveEvent;
@@ -20,6 +23,7 @@ public class MyBrickusFrame extends JFrame {
 
 	JFrame frame;
 	BrickusModel model;
+	FullComponent full;
 	
 	public static void main(String[] args) {
 
@@ -38,9 +42,25 @@ public class MyBrickusFrame extends JFrame {
 		
 		frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setLayout(new GridBagLayout());
+
+		full = new FullComponent(model);
+		frame.getContentPane().add(BorderLayout.CENTER,full);
+		frame.setSize(670,710);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+	}
+}
+
+@SuppressWarnings("serial")
+class FullComponent extends JComponent {
+	BrickusModel myModel;
+	BrickusPiece activePiece;
+	FullComponent(BrickusModel model){
+		myModel = model;
 		GridBagConstraints constraints = new GridBagConstraints();
 		constraints.fill = GridBagConstraints.BOTH;
+		this.setLayout(new GridBagLayout());
+		MyMouseListener myListener = new MyMouseListener(this);
 		
 		MyBrickusBoard board = new MyBrickusBoard(model);
 		board.setBorder(BorderFactory.createLineBorder(Color.black));
@@ -50,16 +70,16 @@ public class MyBrickusFrame extends JFrame {
 		constraints.gridheight = 30;
 		constraints.weightx = 1;
 		constraints.weighty = 1;
-		frame.add(board, constraints);
+		this.add(board, constraints);
 		
-		MyBrickusTray tray = new MyBrickusTray(model); // lower panel of pieces and pass button
+		MyBrickusTray tray = new MyBrickusTray(model, myListener); // lower panel of pieces and pass button
 		constraints.gridx = 0;
 		constraints.gridy = 30;
 		constraints.gridwidth = 1;
 		constraints.gridheight = 9;
 		constraints.weightx = 1;
 		constraints.weighty = 0;
-		frame.add(tray, constraints);
+		this.add(tray, constraints);
 		
 		MyBrickusTracker tracker = new MyBrickusTracker(model); // error board, score boards
 		constraints.gridx = 0;
@@ -68,281 +88,263 @@ public class MyBrickusFrame extends JFrame {
 		constraints.gridheight = 1;
 		constraints.weightx = 1;
 		constraints.weighty = 0;
-		frame.add(tracker, constraints);
-		
-		frame.setSize(670,710);
-		frame.setLocationRelativeTo(null);
-		frame.setVisible(true);
+		this.add(tracker, constraints);
 	}
-}
-
-@SuppressWarnings("serial")
-class MyBrickusBoard extends JComponent {
+   public void pieceClicked(SinglePiece panel) {
+	   	activePiece = panel.selected();
+	   	//I HAVE THE SELECTED PIECE
+	   }
 	
-	int numCol;
-	int numRow;
-	java.util.List<Rectangle> cells;
-	Point coveredCell; // the cell over which the mouse is currently.
-	
-	public MyBrickusBoard(BrickusModel model) {
+	class MyBrickusBoard extends JComponent {
 		
-		numCol = model.getWidth();
-		numRow = model.getHeight() + 1;
-		cells = new ArrayList<>(numCol * numRow);
-	}
-	
-	public void paintComponent(Graphics g) {
+		int numCol;
+		int numRow;
+		java.util.List<Rectangle> cells;
+		Point coveredCell; // the cell over which the mouse is currently.
 		
-		super.paintComponent(g);
-		Graphics2D g2D = (Graphics2D) g.create();
-		
-		int cellWidth = getWidth() / numCol;
-        int cellHeight = getHeight() / numRow;
-        int widthBuffer = (getWidth() - (numCol * cellWidth)) / 2;
-        int heightBuffer = (getHeight() - (numRow * cellHeight)) / 2;
-        
-        if (cells.isEmpty()) {
-            for (int r = 0; r < numRow; r++) {
-                for (int c = 0; c < numCol; c++) {
-                    Rectangle cell = new Rectangle(
-                            widthBuffer + (c * cellWidth),
-                            heightBuffer + (r * cellHeight),
-                            cellWidth,
-                            cellHeight);
-                    cells.add(cell);
-                }
-            }
-        }
-        
-        g2D.setColor(Color.GRAY);
-        for (Rectangle cell : cells) {
-            g2D.draw(cell);
-        }
-	}
-}
-
-@SuppressWarnings("serial")
-class SinglePiece extends JPanel {
-	
-	public SinglePiece(BrickusModel model, BrickusPiece piece) {
-		
-		this.setLayout(new GridLayout(5, 5));
-		this.setBackground(Color.white);
-		
-		int heightBuffer = calculateBuffer(piece.getHeight());
-		int widthBuffer = calculateBuffer(piece.getWidth());
-		System.out.print(piece.getHeight() + " " + heightBuffer + " ");
-		System.out.println(piece.getWidth() + " " + widthBuffer + " ");
-		
-		Player activePlayer = model.getActivePlayer();
-		Color playerColor;
-		if(activePlayer == Player.PLAYER1) {
-			playerColor = Color.blue;
-		}
-		else {
-			playerColor = Color.red;
-		} //REVIVE: put in all 4 Players
-		
-		for(int h=0; h<5; h++) {
+		public MyBrickusBoard(BrickusModel model) {
 			
-			if(h<heightBuffer) {
-				
-				for(int i=0; i<5; i++) {
-					
-					JPanel brick = new JPanel();
-					brick.setBackground(Color.white);
-					this.add(brick);
-				}
+			numCol = model.getWidth();
+			numRow = model.getHeight() + 1;
+			cells = new ArrayList<>(numCol * numRow);
+		}
+		
+		public void paintComponent(Graphics g) {
+			
+			super.paintComponent(g);
+			Graphics2D g2D = (Graphics2D) g.create();
+			
+			int cellWidth = getWidth() / numCol;
+	        int cellHeight = getHeight() / numRow;
+	        int widthBuffer = (getWidth() - (numCol * cellWidth)) / 2;
+	        int heightBuffer = (getHeight() - (numRow * cellHeight)) / 2;
+	        
+	        if (cells.isEmpty()) {
+	            for (int r = 0; r < numRow; r++) {
+	                for (int c = 0; c < numCol; c++) {
+	                    Rectangle cell = new Rectangle(
+	                            widthBuffer + (c * cellWidth),
+	                            heightBuffer + (r * cellHeight),
+	                            cellWidth,
+	                            cellHeight);
+	                    cells.add(cell);
+	                }
+	            }
+	        }
+	        
+	        g2D.setColor(Color.GRAY);
+	        for (Rectangle cell : cells) {
+	            g2D.draw(cell);
+	        }
+		}
+	}
+	
+	class SinglePiece extends JPanel {
+		BrickusPiece mypiece;
+		public SinglePiece(BrickusModel model, BrickusPiece piece, MyMouseListener myListener) {
+			mypiece = piece;
+			this.setLayout(new GridLayout(5, 5));
+			this.setBackground(Color.white);
+			//addListener
+			this.addMouseListener(myListener);
+			int heightBuffer = calculateBuffer(piece.getHeight());
+			int widthBuffer = calculateBuffer(piece.getWidth());
+			System.out.print(piece.getHeight() + " " + heightBuffer + " ");
+			System.out.println(piece.getWidth() + " " + widthBuffer + " ");
+			
+			Player activePlayer = model.getActivePlayer();
+			Color playerColor;
+			if(activePlayer == Player.PLAYER1) {
+				playerColor = Color.blue;
 			}
 			else {
+				playerColor = Color.red;
+			} //REVIVE: put in all 4 Players
+			
+			for(int h=0; h<5; h++) {
 				
-				for(int w=0; w<5; w++) {
+				if(h<heightBuffer) {
 					
-					if(w<widthBuffer) {
+					for(int i=0; i<5; i++) {
 						
 						JPanel brick = new JPanel();
 						brick.setBackground(Color.white);
-						this.add(brick);
-					}
-					else {
-						
-						JPanel brick = new JPanel();
-						if(piece.isOccupied(w-widthBuffer, h-heightBuffer)) {
-						brick.setBackground(playerColor);
-						brick.setBorder(BorderFactory.createLineBorder(Color.black));
-						}
-						else {
-						brick.setBackground(Color.white);
-						}
 						this.add(brick);
 					}
 				}
+				else {
+					
+					for(int w=0; w<5; w++) {
+						
+						if(w<widthBuffer) {
+							
+							JPanel brick = new JPanel();
+							brick.setBackground(Color.white);
+							this.add(brick);
+						}
+						else {
+							
+							JPanel brick = new JPanel();
+							if(piece.isOccupied(w-widthBuffer, h-heightBuffer)) {
+							brick.setBackground(playerColor);
+							brick.setBorder(BorderFactory.createLineBorder(Color.black));
+							}
+							else {
+							brick.setBackground(Color.white);
+							}
+							this.add(brick);
+						}
+					}
+				}
+			}//end for loop h
+		}// end constructor
+		public BrickusPiece selected(){
+			//highlight myself
+			System.out.println("OUCH!");
+			this.setBackground(Color.yellow);
+			return mypiece;
+		}
+		
+		public int calculateBuffer(int pieceDimension) {
+			
+			int buffer = 0;
+			
+			switch(pieceDimension) {
+			case 1:
+				buffer = 2;
+				break;
+			case 2:
+				buffer = 1;
+				break;
+			case 3:
+				buffer = 1;
+				break;
+			case 4:
+				buffer = 0;
+				break;
+			case 5:
+				buffer = 0;
+				break;
+			}
+			
+			return buffer;
+		}
+		
+	}
+	
+	class pieceTray extends JPanel {
+		
+		public pieceTray(BrickusModel model, MyMouseListener mylistener) {
+			
+			this.setLayout(new GridLayout(3, 7));
+			this.setBorder(BorderFactory.createLineBorder(Color.black));
+			
+			for(BrickusPiece piece: model.getPieces(model.getActivePlayer())) {
+				
+				SinglePiece newPiece = new SinglePiece(model, piece, mylistener);
+				this.add(newPiece);
 			}
 		}
 	}
 	
-	public int calculateBuffer(int pieceDimension) {
+	class MyBrickusTray extends JPanel {
+		BrickusPiece mypiece;
 		
-		int buffer = 0;
-		
-		switch(pieceDimension) {
-		case 1:
-			buffer = 2;
-			break;
-		case 2:
-			buffer = 1;
-			break;
-		case 3:
-			buffer = 1;
-			break;
-		case 4:
-			buffer = 0;
-			break;
-		case 5:
-			buffer = 0;
-			break;
+		public MyBrickusTray(BrickusModel model, MyMouseListener mylistener) {
+
+			this.setLayout(new GridBagLayout());
+			this.setBorder(BorderFactory.createLineBorder(Color.black));		
+			GridBagConstraints constraints = new GridBagConstraints();
+			constraints.fill = GridBagConstraints.BOTH;
+			
+			pieceTray pieceTray = new pieceTray(model, mylistener);
+			pieceTray.setBorder(BorderFactory.createLineBorder(Color.black));
+			constraints.gridx = 0;
+			constraints.gridy = 0;
+			constraints.gridwidth = 7;
+			constraints.weightx = 1;
+			this.add(pieceTray, constraints);
+			
+			JButton passButton = new JButton("Pass");
+			constraints.gridx = 7;
+			constraints.gridy = 0;
+			constraints.gridwidth = 1;
+			constraints.weightx = 0;
+			this.add(passButton, constraints);
 		}
-		
-		return buffer;
-	}
+		public void paintComponent(Graphics g) {
 	
-	public class selectPieceHandler implements MouseListener {
-
-		public void mouseClicked(MouseEvent arg0) {
-		
-			
 		}
-
-		public void mouseEntered(MouseEvent arg0) {
-		
-			
-		}
-
-		public void mouseExited(MouseEvent arg0) {
-		
-			
-		}
-
-		public void mousePressed(MouseEvent arg0) {
-		
-			
-		}
-
-		public void mouseReleased(MouseEvent arg0) {
-		
+		public void labelPressed(){
 			
 		}
 	}
-}
-
-@SuppressWarnings("serial")
-class pieceTray extends JPanel {
 	
-	public pieceTray(BrickusModel model) {
+	class MyBrickusTracker extends JPanel {
 		
-		this.setLayout(new GridLayout(3, 7));
-		this.setBorder(BorderFactory.createLineBorder(Color.black));
+		JLabel errorText;
 		
-		for(BrickusPiece piece: model.getPieces(model.getActivePlayer())) {
+		public MyBrickusTracker(BrickusModel model) {
 			
-			SinglePiece newPiece = new SinglePiece(model, piece);
-			this.add(newPiece);
-		}
-	}
-}
-
-@SuppressWarnings("serial")
-class MyBrickusTray extends JPanel {
-	
-	public MyBrickusTray(BrickusModel model) {
-		
-		this.setLayout(new GridBagLayout());
-		this.setBorder(BorderFactory.createLineBorder(Color.black));		
-		GridBagConstraints constraints = new GridBagConstraints();
-		constraints.fill = GridBagConstraints.BOTH;
-		
-		pieceTray pieceTray = new pieceTray(model);
-		pieceTray.setBorder(BorderFactory.createLineBorder(Color.black));
-		constraints.gridx = 0;
-		constraints.gridy = 0;
-		constraints.gridwidth = 7;
-		constraints.weightx = 1;
-		this.add(pieceTray, constraints);
-		
-		JButton passButton = new JButton("Pass");
-		constraints.gridx = 7;
-		constraints.gridy = 0;
-		constraints.gridwidth = 1;
-		constraints.weightx = 0;
-		this.add(passButton, constraints);
-	}
-}
-
-@SuppressWarnings("serial")
-class MyBrickusTracker extends JPanel {
-	
-	JLabel errorText;
-	
-	public MyBrickusTracker(BrickusModel model) {
-		
-		model.addBrickusListener(new ErrorHandler());
-		
-		this.setLayout(new GridBagLayout());
-		this.setBorder(BorderFactory.createLineBorder(Color.black));
-		GridBagConstraints constraints = new GridBagConstraints();
-		constraints.fill = GridBagConstraints.HORIZONTAL;
-		
-		JPanel subpanelLeft = new JPanel(new BorderLayout());
-		JPanel subpanelRight = new JPanel(new FlowLayout(FlowLayout.TRAILING));
-		
-		errorText = new JLabel("<html><font color=000000>Welcome to </font>");
-		errorText.setText(errorText.getText() + "<font color=0000CC>F</font>");
-		errorText.setText(errorText.getText() + "<font color=CC0000>o</font>");
-		errorText.setText(errorText.getText() + "<font color=006600>u</font>");
-		errorText.setText(errorText.getText() + "<font color=FF0066>r</font");
-		errorText.setText(errorText.getText() + "<font color=000000>Cats! =^.^=");
-		
-		JLabel player1Score = new JLabel("<html><font color=0000CC>Score: 0 </font>");
-		JLabel player2Score = new JLabel("<html><font color = CC0000>Score: 0 </font>");
-		JLabel player3Score = new JLabel("<html><font color = 006600>Score: 0 </font>");
-		JLabel player4Score = new JLabel("<html><font color = FF0066>Score: 0 </font>");
-		
-		subpanelLeft.add(errorText);
-		subpanelRight.add(player1Score);
-		subpanelRight.add(player2Score);
-		subpanelRight.add(player3Score);
-		subpanelRight.add(player4Score);
-		
-		constraints.gridx = 0;
-		constraints.gridy = 0;
-		constraints.gridwidth = 5;
-		constraints.weightx = 1;
-		this.add(subpanelLeft, constraints);
-		
-		constraints.gridx = 5;
-		constraints.gridy = 0;
-		constraints.gridwidth = 2;
-		constraints.weightx = 0;
-		this.add(subpanelRight, constraints);
-	}
-	
-	public void changeErrorMessage(String message) {
-		
-		errorText.setText(message);
-	}
-	
-	public class ErrorHandler implements BrickusListener {
-
-		public void illegalMove(BrickusIllegalMoveEvent event) {
-		
-			changeErrorMessage(event.getMessage());
-		}
-
-		public void modelChanged(BrickusEvent arg0) {
-		
-			return;
+			model.addBrickusListener(new ErrorHandler());
+			
+			this.setLayout(new GridBagLayout());
+			this.setBorder(BorderFactory.createLineBorder(Color.black));
+			GridBagConstraints constraints = new GridBagConstraints();
+			constraints.fill = GridBagConstraints.HORIZONTAL;
+			
+			JPanel subpanelLeft = new JPanel(new BorderLayout());
+			JPanel subpanelRight = new JPanel(new FlowLayout(FlowLayout.TRAILING));
+			
+			errorText = new JLabel("<html><font color=000000>Welcome to </font>");
+			errorText.setText(errorText.getText() + "<font color=0000CC>F</font>");
+			errorText.setText(errorText.getText() + "<font color=CC0000>o</font>");
+			errorText.setText(errorText.getText() + "<font color=006600>u</font>");
+			errorText.setText(errorText.getText() + "<font color=FF0066>r</font");
+			errorText.setText(errorText.getText() + "<font color=000000>Cats! =^.^=");
+			
+			JLabel player1Score = new JLabel("<html><font color=0000CC>Score: 0 </font>");
+			JLabel player2Score = new JLabel("<html><font color = CC0000>Score: 0 </font>");
+			JLabel player3Score = new JLabel("<html><font color = 006600>Score: 0 </font>");
+			JLabel player4Score = new JLabel("<html><font color = FF0066>Score: 0 </font>");
+			
+			subpanelLeft.add(errorText);
+			subpanelRight.add(player1Score);
+			subpanelRight.add(player2Score);
+			subpanelRight.add(player3Score);
+			subpanelRight.add(player4Score);
+			
+			constraints.gridx = 0;
+			constraints.gridy = 0;
+			constraints.gridwidth = 5;
+			constraints.weightx = 1;
+			this.add(subpanelLeft, constraints);
+			
+			constraints.gridx = 5;
+			constraints.gridy = 0;
+			constraints.gridwidth = 2;
+			constraints.weightx = 0;
+			this.add(subpanelRight, constraints);
 		}
 		
+		public void changeErrorMessage(String message) {
+			
+			errorText.setText(message);
+		}
 		
+		public class ErrorHandler implements BrickusListener {
+	
+			public void illegalMove(BrickusIllegalMoveEvent event) {
+			
+				changeErrorMessage(event.getMessage());
+			}
+	
+			public void modelChanged(BrickusEvent arg0) {
+			
+				return;
+			}
+			
+			
+		}
 	}
 }
